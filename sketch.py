32c7ad91383499ad9c9edd826fa9659215961ac0
Sankruth Kota <sankruth.kota@gmail.com>
2017-07-19 12:09:21 -0400

    image -> greyscale -> inverted grayscale

--- sketch.py
+++ sketch.py
@@ -1,1 +1,11 @@
 import cv2
+import sys
+import os
+import numpy as np 
+
+#TODO: Request input from user
+input_img = cv2.imread('practice.jpeg', 0)
+input_img = cv2.cvtColor(input_img, cv2.COLOR_RGB2GRAY)
+
+# Take the negative of inputted image
+input_img_negative = cv2.bitwise_not(input_img)
