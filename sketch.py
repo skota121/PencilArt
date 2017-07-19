a0a44ec680b3c9bca7241b1efba81366f88e0acc
Sankruth Kota <sankruth.kota@gmail.com>
2017-07-19 12:45:53 -0400

    fixed the file and probably not going to work with matplotlib for this one

--- sketch.py
+++ sketch.py
@@ -2,17 +2,21 @@ import cv2
 import sys
 import os
 import numpy as np 
-from matplotlib import pyplot as plt
+
+
 #TODO: Request input from user
-input_img = cv2.imread('practice.jpeg', 0)
-input_img = cv2.cvtColor(input_img, cv2.COLOR_RGB2GRAY)
+input_img = cv2.imread('practice.jpeg', 1)
+
+# Convert color image to grayscale
+input_image_gray = cv2.cvtColor(input_img, cv2.COLOR_RGB2GRAY)
 
 # Take the negative of inputted image
-input_img_negative = cv2.bitwise_not(input_img)
+input_img_negative = cv2.bitwise_not(input_image_gray)
 
 # Gaussian Blur the image to smooth any edges
 
-negative_input_blurred = cv2.GuassianBlur(input_img_negative, (5,5), 0)
+negative_input_blurred = cv2.GaussianBlur(input_img_negative, (5,5), 0)
 
-plt.imshow(negative_input_blurred)
-plt.show()
+cv2.imshow('Blurred Image', negative_input_blurred)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
